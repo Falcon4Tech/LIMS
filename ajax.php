a779d1395da62a24127a95258419069ce3f50990
Falcon4Tech <108786187+Falcon4Tech@users.noreply.github.com>
2023-03-27 17:20:16 +0100

    Update

--- ajax.php
+++ ajax.php
@@ -6,11 +6,12 @@ header('Content-type: application/json');
 		if (file_exists("../class/style.{$class}.php")) include("../class/style.{$class}.php");
 	}
 
+	include_once("globals.php");
+
 	$sql_user = "lims_".$_SERVER['REMOTE_USER'];
 	$sql_pass = "dB".$_SERVER['REMOTE_USER']."!#";
 
-	//$mysqli = new log_mysqli('localhost', 'calender', 'call531', 'lims');
-	$mysqli = new log_mysqli('localhost', $sql_user, $sql_pass, 'lims');
+	$mysqli = new log_mysqli(host, $sql_user, $sql_pass, db);
 	$mysqli->debug(true, "lims.ajax");
 	/* check connection */
 	if (mysqli_connect_errno()) {
